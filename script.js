f0bf3a9ab9f9a232aea6c5a535556efff4e84d2b
Ihor <ilss@ukr.net>
2021-02-15 20:57:40 +0200

    firstjava

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-// let pric = 10;
-// console.log(pric);
-// alert(123);
-// let qwerty = confirm('hellow');
-// console.log(qwerty);
-let aa = 9;
---aa;
-console.log(--aa);
+'use strict';
+let money = +prompt("Ваш бюджет на месяц", "");
+let time = prompt("Введите дату в формате YYY-MM-DD", "");
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false};
+
+    let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        a2 = prompt("Во сколько обойдется?", ''),
+        a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        a4 = prompt("Во сколько обойдется?", '');
+    
+    appData.expenses.a1 = a2;
+    appData.expenses.a3 = a4;
+    
+    alert(appData.budget / 30);
