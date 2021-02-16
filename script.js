1c1268e5cf0e87205ab8c9cb4b17744995bde5fd
Ihor <ilss@ukr.net>
2021-02-16 19:21:43 +0200

    160221

--- script.js
+++ script.js
@@ -13,10 +13,16 @@ let appData = {
         a2 = prompt("Во сколько обойдется?", ''),
         a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
         a4 = prompt("Во сколько обойдется?", '');
-    
+    console.log(a1);
+    console.log(a2);
+    console.log(a3);
+    console.log(a4);
     appData.expenses.a1 = a2;
     appData.expenses.a3 = a4;
-    
-    
+    console.log(appData.expenses.a1);
+    console.log(a1);
     alert(appData.budget / 30);
     alert(appData.budget / 30);
+    // if (2+6 = 8) {
+        // console.log("ok")
+    // }
